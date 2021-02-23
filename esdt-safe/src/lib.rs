9138b49361fc694c0dbeb5bb5f6c32008d77a775
dorin.iancu <dorin.iancu@elrond.com>
2021-02-23 13:28:13 +0200

    transaction struct and storage setup + deposit withdrawal logic

--- esdt-safe/src/lib.rs
+++ esdt-safe/src/lib.rs
@@ -1,62 +1,159 @@
 #![no_std]
 
 elrond_wasm::imports!();
+elrond_wasm::derive_imports!();
+
+use elrond_wasm::api::StorageReadApi;
+
+#[derive(TopEncode, TopDecode)]
+pub struct Transaction<BigUint: BigUintApi> {
+    from: Address,
+    to: Address,
+    token: TokenIdentifier,
+    amount: BigUint,
+}
+
+#[derive(TopEncode, TopDecode)]
+pub enum TransactionStatus {
+    None,
+    Pending,
+    InProgress,
+    Executed,
+    Rejected,
+}
 
 #[elrond_wasm_derive::contract(EsdtSafeImpl)]
 pub trait EsdtSafe {
-	#[init]
-	fn init(&self, transaction_fee: BigUint) {
-		self.set_transaction_fee(&transaction_fee);
-	}
+    #[init]
+    fn init(&self, transaction_fee: BigUint, token_whitelist: &[TokenIdentifier]) {
+        self.set_transaction_fee(&transaction_fee);
 
-	// endpoints - owner-only
+        for token in token_whitelist {
+            self.token_whitelist().insert(token.clone());
+        }
+    }
 
-	#[endpoint(setTransactionFee)]
-	fn set_transaction_fee_endpoint(&self, transaction_fee: BigUint) -> SCResult<()> {
-		only_owner!(self, "only owner may call this function");
+    // endpoints - owner-only
 
-		self.set_transaction_fee(&transaction_fee);
+    #[endpoint(setTransactionFee)]
+    fn set_transaction_fee_endpoint(&self, transaction_fee: BigUint) -> SCResult<()> {
+        only_owner!(self, "only owner may call this function");
 
-		Ok(())
-	}
+        self.set_transaction_fee(&transaction_fee);
 
-	// endpoints
+        Ok(())
+    }
 
-	#[payable("EGLD")]
-	#[endpoint(depositEgldForTransactionFee)]
-	fn deposit_egld_for_transaction_fee(&self, #[payment] payment: BigUint) {
-		let caller = self.get_caller();
-		let mut caller_deposit = self.get_deposit(&caller);
-		caller_deposit += payment;
-		self.set_deposit(&caller, &caller_deposit);
-	}
+    #[endpoint(addTokenToWhitelist)]
+    fn add_token_to_whitelist(&self, token_identifier: TokenIdentifier) -> SCResult<()> {
+        only_owner!(self, "only owner may call this function");
 
-	// storage
+        self.token_whitelist().insert(token_identifier);
 
-	// transaction fee, can only be set by owner
+        Ok(())
+    }
 
-	#[view(getTransactionFee)]
-	#[storage_get("transactionFee")]
-	fn get_transaction_fee(&self) -> BigUint;
+    #[endpoint(removeTokenFromWhitelist)]
+    fn remove_token_from_whitelist(&self, token_identifier: TokenIdentifier) -> SCResult<()> {
+        only_owner!(self, "only owner may call this function");
 
-	#[storage_set("transactionFee")]
-	fn set_transaction_fee(&self, transaction_fee: &BigUint);
+        self.token_whitelist().remove(&token_identifier);
 
-	// nonce for each address
+        Ok(())
+    }
 
-	#[view(getAccountNonce)]
-	#[storage_get("accountNonce")]
-	fn get_account_nonce(&self, address: &Address) -> u64;
+    // endpoints
 
-	#[storage_set("accountNonce")]
-	fn set_account_nonce(&self, address: &Address, nonce: u64);
+    #[payable("EGLD")]
+    #[endpoint(depositEgldForTransactionFee)]
+    fn deposit_egld_for_transaction_fee(&self, #[payment] payment: BigUint) {
+        let caller = self.get_caller();
+        let mut caller_deposit = self.get_deposit(&caller);
+        caller_deposit += payment;
+        self.set_deposit(&caller, &caller_deposit);
+    }
 
-	// eGLD amounts deposited by each address, for the sole purpose of paying for transaction fees
+    /// amount argument is optional, defaults to max possible if not provided
+    #[endpoint(whithdrawDeposit)]
+    fn whithdraw_deposit(&self, #[var_args] opt_amount: OptionalArg<BigUint>) -> SCResult<()> {
+        let caller = self.get_caller();
+        let caller_deposit = self.get_deposit(&caller);
+        let amount = match opt_amount {
+            OptionalArg::Some(value) => value,
+            OptionalArg::None => caller_deposit.clone(),
+        };
 
-	#[view(getDeposit)]
-	#[storage_get("deposit")]
-	fn get_deposit(&self, address: &Address) -> BigUint;
+        require!(amount <= caller_deposit, "Trying to whithdraw too much");
 
-	#[storage_set("deposit")]
-	fn set_deposit(&self, address: &Address, deposit: &BigUint);
+        let deposit_remaining = &caller_deposit - &amount;
+        self.send().direct_egld(&caller, &amount, b"whitdrawal");
+        self.set_deposit(&caller, &deposit_remaining);
+
+        Ok(())
+    }
+
+    // storage
+
+    // transaction fee, can only be set by owner
+
+    #[view(getTransactionFee)]
+    #[storage_get("transactionFee")]
+    fn get_transaction_fee(&self) -> BigUint;
+
+    #[storage_set("transactionFee")]
+    fn set_transaction_fee(&self, transaction_fee: &BigUint);
+
+    // token whitelist
+
+    #[storage_mapper("tokenWhitelist")]
+    fn token_whitelist(&self) -> SetMapper<Self::Storage, TokenIdentifier>;
+
+    // nonce for each address
+
+    #[view(getAccountNonce)]
+    #[storage_get("accountNonce")]
+    fn get_account_nonce(&self, address: &Address) -> usize;
+
+    #[storage_set("accountNonce")]
+    fn set_account_nonce(&self, address: &Address, nonce: usize);
+
+    // eGLD amounts deposited by each address, for the sole purpose of paying for transaction fees
+
+    #[view(getDeposit)]
+    #[storage_get("deposit")]
+    fn get_deposit(&self, address: &Address) -> BigUint;
+
+    #[storage_set("deposit")]
+    fn set_deposit(&self, address: &Address, deposit: &BigUint);
+
+    // transactions for each address, sorted by nonce
+    // due to how VecMapper works internally, nonces will start at 1
+
+    #[storage_mapper("transactionsByNonce")]
+    fn transactions_by_nonce(
+        &self,
+        address: &Address,
+    ) -> VecMapper<Self::Storage, Transaction<BigUint>>;
+
+    #[storage_get("transactionStatus")]
+    fn get_transaction_status(&self, sender: &Address, nonce: usize) -> TransactionStatus;
+
+    #[storage_set("transactionStatus")]
+    fn set_transaction_status(
+        &self,
+        sender: &Address,
+        nonce: usize,
+        transaction_status: TransactionStatus,
+    );
+
+	#[storage_mapper("pendingTransactionList")]
+	fn pending_transaction_list(&self) -> LinkedListMapper<Self::Storage, (Address, usize)>;
+
+    // TODO: Remove in the next patch, VecMapper will have a len() method then
+
+    fn get_transactions_by_nonce_len(&self, address: &Address) -> usize {
+        self.get_storage_raw()
+            .storage_load_u64(&[b"transactionsByNonce", address.as_bytes()].concat())
+            as usize
+    }
 }
