9571028989d7573412b61962f26995116f1aacb2
dorin.iancu <dorin.iancu@elrond.com>
2021-03-25 11:59:12 +0200

    snippets fixes

--- egld-esdt-swap/interaction/snippets.sh
+++ egld-esdt-swap/interaction/snippets.sh
@@ -6,7 +6,7 @@ CHAIN_ID=local-testnet
 
 TOKEN_DISPLAY_NAME=0x5772617070656445676c64  # "WrappedEgld"
 TOKEN_TICKER=0x5745474c44  # "WEGLD"
-TOKEN_IDENTIFIER=0x # Manually update after issue
+TOKEN_IDENTIFIER=0x5745474c442d383864366361 # Manually update after issue
 
 UNWRAP_EGLD_ENDPOINT=0x756e7772617045676c64
 
@@ -28,11 +28,11 @@ upgrade() {
 }
 
 issueWrappedEgld() {
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=80000000 --value=5000000000000000000 --function="issueWrappedEgld" --arguments ${TOKEN_DISPLAY_NAME} ${TOKEN_TICKER} 0x03E8 --send --proxy=${PROXY} --chain=${CHAIN_ID}
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=60000000 --value=5000000000000000000 --function="issueWrappedEgld" --arguments ${TOKEN_DISPLAY_NAME} ${TOKEN_TICKER} 0x03E8 --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
 setLocalMintRole() {
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=500000000 --function="setLocalMintRole" --send --proxy=${PROXY} --chain=${CHAIN_ID}
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=60000000 --function="setLocalMintRole" --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
 localMint() {
@@ -40,11 +40,11 @@ localMint() {
 }
 
 wrapEgld() {
-    erdpy --verbose contract call ${ADDRESS} --nonce=${alice_nonce} --pem=${ALICE} --gas-limit=1000000000 --function="wrapEgld" --value=0x0A --send --proxy=${PROXY} --chain=${CHAIN_ID}
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=10000000 --value=0x0A --function="wrapEgld" --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
 unwrapEgld() {
-    erdpy --verbose contract call ${ADDRESS} --nonce=${alice_nonce} --pem=${ALICE} --gas-limit=1000000000 --function="ESDTTransfer" --arguments ${TOKEN_IDENTIFIER} 0x05 ${UNWRAP_EGLD_ENDPOINT} --send --proxy=${PROXY} --chain=${CHAIN_ID}
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=1000000000 --function="ESDTTransfer" --arguments ${TOKEN_IDENTIFIER} 0x05 ${UNWRAP_EGLD_ENDPOINT} --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
 # views
