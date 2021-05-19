86efd70c8800962356312323d1799923a80e3239
dorin.iancu <dorin.iancu@elrond.com>
2021-05-19 15:59:52 +0300

    EsdtSafe snippets update

--- egld-esdt-swap/interaction/snippets.sh
+++ egld-esdt-swap/interaction/snippets.sh
@@ -2,8 +2,8 @@ ALICE="/home/elrond/elrond-sdk/erdpy/testnet/wallets/users/alice.pem"
 BOB="/home/elrond/elrond-sdk/erdpy/testnet/wallets/users/bob.pem"
 ADDRESS=$(erdpy data load --key=address-testnet)
 DEPLOY_TRANSACTION=$(erdpy data load --key=deployTransaction-testnet)
-PROXY=http://localhost:7950
-CHAIN_ID=local-testnet
+PROXY=https://testnet-gateway.elrond.com
+CHAIN_ID=T
 
 ESDT_SYSTEM_SC_ADDRESS=erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u
 
@@ -11,7 +11,7 @@ deploy() {
     ######################################################################
     ############################ Update after issue ######################
     ######################################################################
-    WRAPPED_EGLD_TOKEN_ID=0x
+    local WRAPPED_EGLD_TOKEN_ID=0x
 
     erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} --gas-limit=100000000 --arguments ${WRAPPED_EGLD_TOKEN_ID} --send --outfile="deploy-testnet.interaction.json" --proxy=${PROXY} --chain=${CHAIN_ID} || return
 
@@ -30,19 +30,19 @@ upgrade() {
 }
 
 issueWrappedEgld() {
-    TOKEN_DISPLAY_NAME=0x5772617070656445676c64  # "WrappedEgld"
-    TOKEN_TICKER=0x5745474c44  # "WEGLD"
-    INITIAL_SUPPLY=0x01 # 1
-    NR_DECIMALS=0x12 # 18
-    CAN_ADD_SPECIAL_ROLES=0x63616e4164645370656369616c526f6c6573 # "canAddSpecialRoles"
-    TRUE=0x74727565 # "true"
+    local TOKEN_DISPLAY_NAME=0x5772617070656445676c64  # "WrappedEgld"
+    local TOKEN_TICKER=0x5745474c44  # "WEGLD"
+    local INITIAL_SUPPLY=0x01 # 1
+    local NR_DECIMALS=0x12 # 18
+    local CAN_ADD_SPECIAL_ROLES=0x63616e4164645370656369616c526f6c6573 # "canAddSpecialRoles"
+    local TRUE=0x74727565 # "true"
 
     erdpy --verbose contract call ${ESDT_SYSTEM_SC_ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=60000000 --value=5000000000000000000 --function="issue" --arguments ${TOKEN_DISPLAY_NAME} ${TOKEN_TICKER} ${INITIAL_SUPPLY} ${NR_DECIMALS} ${CAN_ADD_SPECIAL_ROLES} ${TRUE} --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
 setLocalRoles() {
-    LOCAL_MINT_ROLE=0x45534454526f6c654c6f63616c4d696e74 # "ESDTRoleLocalMint"
-    LOCAL_BURN_ROLE=0x45534454526f6c654c6f63616c4275726e # "ESDTRoleLocalBurn"
+    local LOCAL_MINT_ROLE=0x45534454526f6c654c6f63616c4d696e74 # "ESDTRoleLocalMint"
+    local LOCAL_BURN_ROLE=0x45534454526f6c654c6f63616c4275726e # "ESDTRoleLocalBurn"
 
     erdpy --verbose contract call ${ESDT_SYSTEM_SC_ADDRESS} --recall-nonce --pem=${ALICE} --gas-limit=60000000 --function="setSpecialRole" --arguments ${WRAPPED_EGLD_TOKEN_ID} ${ADDRESS} ${LOCAL_MINT_ROLE} ${LOCAL_BURN_ROLE} --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
@@ -52,8 +52,8 @@ wrapEgldBob() {
 }
 
 unwrapEgldBob() {
-    UNWRAP_EGLD_ENDPOINT=0x756e7772617045676c64 # "unwrapEgld"
-    UNWRAP_AMOUNT=0x05
+    local UNWRAP_EGLD_ENDPOINT=0x756e7772617045676c64 # "unwrapEgld"
+    local UNWRAP_AMOUNT=0x05
 
     getWrappedEgldTokenIdentifier
     erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${BOB} --gas-limit=10000000 --function="ESDTTransfer" --arguments ${TOKEN_IDENTIFIER} ${UNWRAP_AMOUNT} ${UNWRAP_EGLD_ENDPOINT} --send --proxy=${PROXY} --chain=${CHAIN_ID}
