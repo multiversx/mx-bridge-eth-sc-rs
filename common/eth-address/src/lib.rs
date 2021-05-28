b545db7370c69b84163d631b1c36b965d2d46879
dorin.iancu <dorin.iancu@elrond.com>
2021-05-28 13:18:30 +0300

    esdt safe create tx test + use Box for EthAddress type

--- common/eth-address/src/lib.rs
+++ common/eth-address/src/lib.rs
@@ -1,15 +1,16 @@
 #![no_std]
 
 elrond_wasm::derive_imports!();
+use elrond_wasm::types::Box;
 
 pub const ETH_ADDRESS_LEN: usize = 20;
 
 #[derive(TypeAbi, TopEncode, TopDecode, NestedEncode, NestedDecode, Clone)]
-pub struct EthAddress([u8; ETH_ADDRESS_LEN]);
+pub struct EthAddress(Box<[u8; ETH_ADDRESS_LEN]>);
 
 impl EthAddress {
     pub fn as_slice(&self) -> &[u8] {
-        &self.0[..]
+        &(*self.0)[..]
     }
 
     pub fn is_zero(&self) -> bool {
@@ -19,7 +20,7 @@ impl EthAddress {
 
 impl EthAddress {
     pub fn zero() -> Self {
-        EthAddress([0u8; ETH_ADDRESS_LEN])
+        EthAddress(Box::from([0u8; ETH_ADDRESS_LEN]))
     }
 }
 
@@ -27,7 +28,7 @@ impl<'a> From<&'a [u8]> for EthAddress {
     fn from(slice: &'a [u8]) -> Self {
         let mut zero = Self::zero();
         if slice.len() == ETH_ADDRESS_LEN {
-            zero.0.copy_from_slice(&slice)
+            (*zero.0).copy_from_slice(&slice)
         }
 
         zero
