8e4b1511bb755bf787d97e99e152ea02044bba22
Catalin Neagu <neagu.catalin.f@gmail.com>
2021-07-18 18:11:33 +0300

    transfer with async call when it needs execution

--- egld-esdt-swap/src/lib.rs
+++ egld-esdt-swap/src/lib.rs
@@ -1,7 +1,6 @@
 #![no_std]
 
 elrond_wasm::imports!();
-const DEFAULT_GAS_LEFTOVER: u64 = 100_000;
 
 #[elrond_wasm_derive::contract]
 pub trait EgldEsdtSwap {
@@ -25,7 +24,7 @@ pub trait EgldEsdtSwap {
         &self,
         #[payment] payment: Self::BigUint,
         #[var_args] accept_funds_endpoint_name: OptionalArg<BoxedBytes>,
-    ) -> SCResult<()> {
+    ) -> SCResult<OptionalResult<AsyncCall<Self::SendApi>>> {
         require!(payment > 0, "Payment must be more than 0");
 
         let wrapped_egld_token_id = self.wrapped_egld_token_id().get();
@@ -37,17 +36,20 @@ pub trait EgldEsdtSwap {
         let caller = self.blockchain().get_caller();
         let function = accept_funds_endpoint_name
             .into_option()
-            .unwrap_or_else(|| b""[..].into());
-        let gas_limit = self.gas_limit_with_leftover();
+            .unwrap_or_else(BoxedBytes::empty);
 
-        SCResult::from_result(self.send().direct_esdt_execute(
-            &caller,
-            &wrapped_egld_token_id,
-            &payment,
-            gas_limit,
-            function.as_slice(),
-            &ArgBuffer::new(),
-        ))
+        if self.needs_execution(&caller, &function) {
+            Ok(OptionalResult::Some(self.transfer_and_execute_via_async(
+                &caller,
+                &wrapped_egld_token_id,
+                &payment,
+                &function,
+            )))
+        } else {
+            self.send()
+                .direct(&caller, &wrapped_egld_token_id, &payment, b"wrapping");
+            Ok(OptionalResult::None)
+        }
     }
 
     #[payable("*")]
@@ -57,7 +59,7 @@ pub trait EgldEsdtSwap {
         #[payment] payment: Self::BigUint,
         #[payment_token] token_id: TokenIdentifier,
         #[var_args] accept_funds_endpoint_name: OptionalArg<BoxedBytes>,
-    ) -> SCResult<()> {
+    ) -> SCResult<OptionalResult<AsyncCall<Self::SendApi>>> {
         let wrapped_egld_token_id = self.wrapped_egld_token_id().get();
 
         require!(token_id.is_esdt(), "Only ESDT tokens accepted");
@@ -77,16 +79,20 @@ pub trait EgldEsdtSwap {
         let caller = self.blockchain().get_caller();
         let function = accept_funds_endpoint_name
             .into_option()
-            .unwrap_or_else(|| b""[..].into());
-        let gas_limit = self.gas_limit_with_leftover();
+            .unwrap_or_else(BoxedBytes::empty);
 
-        SCResult::from_result(self.send().direct_egld_execute(
-            &caller,
-            &payment,
-            gas_limit,
-            function.as_slice(),
-            &ArgBuffer::new(),
-        ))
+        if self.needs_execution(&caller, &function) {
+            Ok(OptionalResult::Some(self.transfer_and_execute_via_async(
+                &caller,
+                &TokenIdentifier::egld(),
+                &payment,
+                &function,
+            )))
+        } else {
+            self.send()
+                .direct(&caller, &TokenIdentifier::egld(), &payment, b"unwrapping");
+            Ok(OptionalResult::None)
+        }
     }
 
     // views
@@ -98,31 +104,55 @@ pub trait EgldEsdtSwap {
 
     // private
 
-    fn gas_limit_with_leftover(&self) -> u64 {
-        let gas_left = self.blockchain().get_gas_left();
-        if gas_left > DEFAULT_GAS_LEFTOVER {
-            gas_left - DEFAULT_GAS_LEFTOVER
-        } else {
-            gas_left
-        }
+    fn needs_execution(&self, caller: &Address, function: &BoxedBytes) -> bool {
+        self.blockchain().is_smart_contract(caller) && !function.is_empty()
     }
 
-    fn data_or_empty(&self, to: &Address, data: &'static [u8]) -> &[u8] {
-        if self.blockchain().is_smart_contract(to) {
-            &[]
-        } else {
-            data
-        }
+    fn transfer_and_execute_via_async(
+        &self,
+        caller: &Address,
+        token_id: &TokenIdentifier,
+        amount: &Self::BigUint,
+        function: &BoxedBytes,
+    ) -> AsyncCall<Self::SendApi> {
+        let contract_call: ContractCall<Self::SendApi, ()> =
+            ContractCall::new(self.send(), caller.clone(), function.clone())
+                .with_token_transfer(token_id.clone(), amount.clone());
+
+        contract_call
+            .async_call()
+            .with_callback(self.callbacks().async_transfer_execute_callback(caller))
     }
 
-    fn require_local_role_set(&self, token_id: &TokenIdentifier, role: &EsdtLocalRole) -> SCResult<()> {
+    fn require_local_role_set(
+        &self,
+        token_id: &TokenIdentifier,
+        role: &EsdtLocalRole,
+    ) -> SCResult<()> {
         let roles = self.blockchain().get_esdt_local_roles(token_id);
-        require!(
-            roles.contains(role),
-            "Must set local role first"
-        );
+        require!(roles.contains(role), "Must set local role first");
 
         Ok(())
+    }
+
+    // callbacks
+
+    #[callback]
+    fn async_transfer_execute_callback(
+        &self,
+        caller: &Address,
+        #[call_result] result: AsyncCallResult<()>,
+    ) {
+        match result {
+            AsyncCallResult::Ok(_) => {}
+            AsyncCallResult::Err(_) => {
+                let (returned_tokens, token_identifier) = self.call_value().payment_token_pair();
+                if returned_tokens > 0 {
+                    self.send()
+                        .direct(caller, &token_identifier, &returned_tokens, &[]);
+                }
+            }
+        }
     }
 
     // storage
