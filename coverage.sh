f76fb03bf01d3cc23a3cca9b2a45ddc6724cd037
dorin.iancu <dorin.iancu@elrond.com>
2021-10-25 14:53:45 +0300

    fix coverage script

--- coverage.sh
+++ coverage.sh
@@ -2,6 +2,7 @@ export CARGO_INCREMENTAL=0
 export RUSTFLAGS="-Zprofile -Ccodegen-units=1 -Copt-level=0 -Clink-dead-code -Coverflow-checks=off -Zpanic_abort_tests"
 export RUSTDOCFLAGS="-Cpanic=abort"
 
+cargo clean
 cargo build
 cargo test
 
