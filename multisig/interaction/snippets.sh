28c39f4508444214fbbc70074dc3e42c43bdfdfd
Cristi Corcoveanu <cristi.corcoveanu@gmail.com>
2021-10-25 11:00:49 +0300

    latest snippet changes

--- multisig/interaction/snippets.sh
+++ multisig/interaction/snippets.sh
@@ -3,15 +3,22 @@
 # Quorum size will be 1
 
 # Path towards PEM files
-<<<<<<< Updated upstream
-ALICE="/home/elrond/Downloads/devnetWalletKey.pem"
-BOB=""
-=======
+PROJECT="../"
 ALICE="./wallets/alice.pem"
 BOB="./wallets/bob.pem"
->>>>>>> Stashed changes
 
-ADDRESS=erd1qqqqqqqqqqqqqpgq5300tayry6ardyw66azx3tljp3uhl8jq082sluzkm4
+SHARD0="./walletsRelay/walletKey0.pem"
+SHARD1="./walletsRelay/walletKey1.pem"
+SHARD2="./walletsRelay/walletKey2.pem"
+SHARD3="./walletsRelay/walletKey3.pem"
+SHARD4="./walletsRelay/walletKey4.pem"
+SHARD5="./walletsRelay/walletKey5.pem"
+SHARD6="./walletsRelay/walletKey6.pem"
+SHARD7="./walletsRelay/walletKey7.pem"
+SHARD8="./walletsRelay/walletKey8.pem"
+SHARD9="./walletsRelay/walletKey9.pem"
+
+ADDRESS=$(erdpy data load --key=address-testnet-multisig)
 DEPLOY_TRANSACTION=$(erdpy data load --key=deployTransaction-testnet)
 PROXY=https://devnet-gateway.elrond.com
 CHAIN_ID=D
@@ -21,18 +28,17 @@ ESDT_ISSUE_COST=0xB1A2BC2EC50000 # 0.05 eGLD
 ESDT_ISSUE_COST_DECIMAL=50000000000000000
 
 # Addresses in Hex
-BOB_ADDRESS=0x
-
-RELAYER_ADDR_0=0x5cc00bb6d62665482fb7a98f688de4576908b3d86bb6b905786c50e9a6ca3493
-RELAYER_ADDR_1=0x316df040b2377b904ca7287d72f1445690e399dbacd1e6387bd72fd23f790c03
-RELAYER_ADDR_2=0x861414a440f506b5e728e6367083b9eb78af2707645146d8c387471290b01c55
-RELAYER_ADDR_3=0x15788d4dada25be9aaf8f9db773738ececedd379d94d69ac1773a70a5e969c36
-RELAYER_ADDR_4=0x61d4fea8d1f876dd8f5ce7c12b4d68059798aa639331999b5f130973dfa711e3
-RELAYER_ADDR_5=0xcb56df4813f7db30010fb3b9bc0713785c20738d7bca4028fc3840d9c7fbeb58
-RELAYER_ADDR_6=0xc1d3fb6ee84b9b2ffef639f18cda542dfab5bfa86f1b6e82f6c9bc9283e695f3
-RELAYER_ADDR_7=0xebe11b66f2d641c161ab02fcec75c0d1b5b883c111246d63d3293583d2b15081
-RELAYER_ADDR_8=0x25b9743889b9c6b3ab8409adda606a39183dd6f2e6edf48a43b8c42dfeebb45f
-RELAYER_ADDR_9=0x9b7971db47e3815a669a91c3f1bcb21e0b81f2de04bf11faa7a34b9b10e7cfbb
+BOB_ADDRESS=0x8049d639e5a6980d1cd2392abcce41029cda74a1563523a202f09641cc2618f8 #erd1spyavw0956vq68xj8y4tenjpq2wd5a9p2c6j8gsz7ztyrnpxrruqzu66jx
+RELAYER_ADDR_0=0x7ae98777615241f87c97c2c4dcae2c70e20c8ad6b18a82459fe483e2682fa4e5 #erd10t5cwamp2fqlslyhctzdet3vwr3qezkkkx9gy3vlujp7y6p05njsl8npv9
+RELAYER_ADDR_1=0x53490a678ea9cccfe6c803a75af670a3026f2e9ed373f1e4e771f170014f5d53 #erd12dys5euw48xvlekgqwn44ans5vpx7t576delre88w8chqq20t4fs3njzv6
+RELAYER_ADDR_2=0x26e3269a51bac82cc458c2059a002260ab46bda8f8182b73a6ec645fb7694b97 #erd1ym3jdxj3htyze3zccgze5qpzvz45d0dglqvzkuaxa3j9ldmffwts3hsaj0
+RELAYER_ADDR_3=0xc7e98c4ca730f80240e7e5ef8c387607f85685130389705b528800d586770c0d #erd1cl5ccn98xruqys88uhhccwrkqlu9dpgnqwyhqk6j3qqdtpnhpsxsad3dxd
+RELAYER_ADDR_4=0x522279c5aed5b49a2a6d8ebef5d1f79d03c96f4fe70296e1605bd8ccd91d2010 #erd12g38n3dw6k6f52nd36l0t50hn5pujm60uupfdctqt0vvekgayqgq8pm7z8
+RELAYER_ADDR_5=0x6e19c24c9ada16cf03e616caf53958d5089ee57022d46e8faaac7cdca0c639a8 #erd1dcvuyny6mgtv7qlxzm902w2c65yfaetsyt2xara2437degxx8x5qpxssnh
+RELAYER_ADDR_6=0xd285e3bbaef774ec5ef8e9e9fe1236f95e16763ed1c6e47acfa93a6efdac2f20 #erd162z78waw7a6wchhca85luy3kl90pva3768rwg7k04yaxaldv9usq0tpf5y
+RELAYER_ADDR_7=0x546a03d52943d15d7913edc545b03e7ef70aca259cc88f1a916a57873e1b6518 #erd1234q84ffg0g467gnahz5tvp70mms4j39nnyg7x53dftcw0smv5vqs72j58
+RELAYER_ADDR_8=0x85c78a25accc17768168bc384787adadde105b8836d49b3d24189ef0e5f4a0f3 #erd1shrc5fdvesthdqtghsuy0pad4h0pqkugxm2fk0fyrz00pe055ress3adjq
+RELAYER_ADDR_9=0x5a83644fe1de271497cd5159615f60b212936532c71fb134eeaf9cf38f2f4d63 #erd1t2pkgnlpmcn3f97d29vkzhmqkgffxefjcu0mzd8w47w08re0f43sn9vtew
 
 ESDT_SYSTEM_SC_ADDRESS=erd1qqqqqqqqqqqqqqqpqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqzllls8a5w6u
 
@@ -42,16 +48,19 @@ AGGREGATOR_ADDRESS=0x0000000000000000050081d0b65d6bd5bd7d5af6df1a26e89513c6f38cd
 #########################################################################
 ################## Update after issueing the tokens #####################
 #########################################################################
-WRAPPED_EGLD_TOKEN_ID=0x45474c442d316138626639
-WRAPPED_ETH_TOKEN_ID=0x4554482d353063336133
+WRAPPED_EGLD_TOKEN_ID=0x45474c442d323237373162
+WRAPPED_ETH_TOKEN_ID=0x4554482d653633623166
 
 deploy() {
     local SLASH_AMOUNT=0x0a # 1
 
     erdpy --verbose contract deploy --project=${PROJECT} --recall-nonce --pem=${ALICE} \
     --gas-limit=400000000 \
-    --arguments ${RELAYER_REQUIRED_STAKE} ${SLASH_AMOUNT} 0x02 \
+    --arguments ${RELAYER_REQUIRED_STAKE} ${SLASH_AMOUNT} 0x03 \
+    ${RELAYER_ADDR_0} \
     ${RELAYER_ADDR_1} ${RELAYER_ADDR_2} ${RELAYER_ADDR_3} \
+    ${RELAYER_ADDR_4} ${RELAYER_ADDR_5} ${RELAYER_ADDR_6} \
+    ${RELAYER_ADDR_7} ${RELAYER_ADDR_8} ${RELAYER_ADDR_9} \
     --send --outfile="deploy-testnet.interaction.json" --proxy=${PROXY} --chain=${CHAIN_ID} || return
 
     TRANSACTION=$(erdpy data parse --file="./deploy-testnet.interaction.json" --expression="data['emitted_tx']['hash']")
@@ -70,9 +79,9 @@ upgrade() {
 }
 
 deployChildContracts() {
-    local EGLD_ESDT_SWAP_CODE=0x"$(xxd -p ../egld-esdt-swap/output/egld-esdt-swap.wasm | tr -d '\n')"
-    local ESDT_SAFE_CODE=0x"$(xxd -p ../esdt-safe/output/esdt-safe.wasm | tr -d '\n')"
-    local MULTI_TRANSFER_ESDT_CODE=0x"$(xxd -p ../multi-transfer-esdt/output/multi-transfer-esdt.wasm | tr -d '\n')"
+    local EGLD_ESDT_SWAP_CODE=0x"$(xxd -p ../../egld-esdt-swap/output/egld-esdt-swap.wasm | tr -d '\n')"
+    local ESDT_SAFE_CODE=0x"$(xxd -p ../../esdt-safe/output/esdt-safe.wasm | tr -d '\n')"
+    local MULTI_TRANSFER_ESDT_CODE=0x"$(xxd -p ../../multi-transfer-esdt/output/multi-transfer-esdt.wasm | tr -d '\n')"
 
     local ESDT_SAFE_ETH_TX_GAS_LIMIT=150000
     local MULTI_TRANSFER_ESDT_TX_GAS_LIMIT=10000
@@ -88,8 +97,17 @@ deployChildContracts() {
 stake() {
     local RELAYER_REQUIRED_STAKE_DECIMAL=1000
 
-    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${BOB} \
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${SHARD9} \
     --gas-limit=35000000 --function="stake" --value=${RELAYER_REQUIRED_STAKE_DECIMAL} \
+    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+}
+
+addTokenToWhitelist() {
+    local RELAYER_REQUIRED_STAKE_DECIMAL=1000
+
+    erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} \
+    --gas-limit=60000000 --function="esdtSafeAddTokenToWhitelist" \
+    --arguments ${WRAPPED_EGLD_TOKEN_ID} \
     --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
@@ -101,8 +119,8 @@ unstake() {
 }
 
 addMapping() {
-    local WRAPPED_EGLD_ERC20=0xC06606b0248F56aA93DB3236dB0bED97B9Ad1135
-    local WRAPPED_ETH_ERC20=0x1F3ff2dA93DB23be6F73696950701F5cE471D7d4
+    local WRAPPED_EGLD_ERC20=0x64a8bfab8e7ac5d5a3561d95b504542e9e29ce24
+    local WRAPPED_ETH_ERC20=0x4d75EF4411cda0E0C257383054Fe68febB993D37
 
     erdpy --verbose contract call ${ADDRESS} --recall-nonce --pem=${ALICE} \
     --gas-limit=40000000 --function="addMapping" \
@@ -131,7 +149,7 @@ changeQuorum() {
 issueWrappedEgld() {
     local TOKEN_DISPLAY_NAME=0x5772617070656445676c64  # "WrappedEgld"
     local TOKEN_TICKER=0x45474c44  # "EGLD"
-    local INITIAL_SUPPLY=0x01 # 1
+    local INITIAL_SUPPLY=0x00 # 1
     local NR_DECIMALS=0x12 # 18
     local CAN_ADD_SPECIAL_ROLES=0x63616e4164645370656369616c526f6c6573 # "canAddSpecialRoles"
     local TRUE=0x74727565 # "true"
@@ -145,7 +163,7 @@ issueWrappedEgld() {
 issueWrappedEth() {
     local TOKEN_DISPLAY_NAME=0x57726170706564457468  # "WrappedEth"
     local TOKEN_TICKER=0x455448  # "ETH"
-    local INITIAL_SUPPLY=0x01 # 1
+    local INITIAL_SUPPLY=0x00 # 1
     local NR_DECIMALS=0x12 # 18
     local CAN_ADD_SPECIAL_ROLES=0x63616e4164645370656369616c526f6c6573 # "canAddSpecialRoles"
     local TRUE=0x74727565 # "true"
