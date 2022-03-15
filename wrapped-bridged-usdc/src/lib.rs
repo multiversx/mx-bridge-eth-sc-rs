ca571aa8d72d7f5be18c0229f24ce13605f58d4d
Alin Cruceat <alyn_509@yahoo.com>
2022-03-15 12:14:10 +0200

    usdc_token_id as UnorderedSetMapper

--- wrapped-bridged-usdc/src/lib.rs
+++ wrapped-bridged-usdc/src/lib.rs
@@ -7,11 +7,15 @@ const LEFTOVER_GAS: u64 = 10_000u64;
 #[elrond_wasm::derive::contract]
 pub trait WrappedBridgedUsdc {
     #[init]
-    fn init(&self, usdc_token_id: TokenIdentifier, wrapped_usdc_token_id: TokenIdentifier) {
-        self.usdc_token_id().set(&usdc_token_id);
-        self.wrapped_usdc_token_id().set(&wrapped_usdc_token_id);
+    fn init(&self, wrapped_usdc_token_id: TokenIdentifier) {
+        self.wrapped_usdc_token_id().set(wrapped_usdc_token_id);
     }
 
+    #[only_owner]
+    #[endpoint(whitelistUSDC)]
+    fn whitelist_usdc(&self, usdc_token_id: TokenIdentifier) {
+        self.usdc_token_id().insert(usdc_token_id);
+    }
     // endpoints
 
     #[payable("*")]
@@ -22,10 +26,13 @@ pub trait WrappedBridgedUsdc {
         #[payment_amount] payment_amount: BigUint,
         #[var_args] accept_funds_endpoint_name: OptionalValue<ManagedBuffer>,
     ) {
-        let usdc_token_id = self.usdc_token_id().get();
+        let usdc_token_id = &payment_token;
         let wrapped_usdc_token_id = self.wrapped_usdc_token_id().get();
 
-        require!(payment_token == usdc_token_id, "Wrong esdt token");
+        require!(
+            self.usdc_token_id().contains(usdc_token_id),
+            "Wrong esdt token"
+        );
         require!(payment_amount > 0u32, "Payment must be more than 0");
 
         self.send()
@@ -59,23 +66,28 @@ pub trait WrappedBridgedUsdc {
         &self,
         #[payment_token] payment_token: TokenIdentifier,
         #[payment_amount] payment_amount: BigUint,
+        requested_token: TokenIdentifier,
         #[var_args] accept_funds_endpoint_name: OptionalValue<ManagedBuffer>,
     ) {
-        let usdc_token_id = self.usdc_token_id().get();
+        let usdc_token_id = &requested_token;
         let wrapped_usdc_token_id = self.wrapped_usdc_token_id().get();
 
         require!(payment_token == wrapped_usdc_token_id, "Wrong esdt token");
+        require!(
+            self.usdc_token_id().contains(usdc_token_id),
+            "Esdt token unavailable"
+        );
         require!(payment_amount > 0u32, "Must pay more than 0 tokens!");
         // this should never happen, but we'll check anyway
         require!(
-            payment_amount <= self.get_locked_balance(&usdc_token_id),
+            payment_amount <= self.get_liquidity(&usdc_token_id),
             "Contract does not have enough funds"
         );
 
         self.send()
             .esdt_local_burn(&wrapped_usdc_token_id, 0, &payment_amount);
 
-        // 1 wrapped eGLD = 1 eGLD, so we pay back the same amount
+        // 1 wrapped USDC = 1 USDC, so we pay back the same amount
         let caller = self.blockchain().get_caller();
         let function = match accept_funds_endpoint_name {
             OptionalValue::Some(f) => f,
@@ -100,8 +112,8 @@ pub trait WrappedBridgedUsdc {
 
     // views
 
-    #[view(getLockedUSDCBalance)]
-    fn get_locked_balance(&self, usdc_token: &TokenIdentifier) -> BigUint {
+    #[view(getLiquidity)]
+    fn get_liquidity(&self, usdc_token: &TokenIdentifier) -> BigUint {
         self.blockchain().get_sc_balance(usdc_token, 0)
     }
 
@@ -121,5 +133,5 @@ pub trait WrappedBridgedUsdc {
 
     #[view(getUSDCTokenId)]
     #[storage_mapper("USDCTokenId")]
-    fn usdc_token_id(&self) -> SingleValueMapper<TokenIdentifier>;
+    fn usdc_token_id(&self) -> UnorderedSetMapper<TokenIdentifier>;
 }
