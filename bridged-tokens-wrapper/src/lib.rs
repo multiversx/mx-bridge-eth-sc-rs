0d848222c7cba8389f8b9e89e58ea1c53cb6e093
dorin.iancu <dorin.iancu@elrond.com>
2022-03-18 14:38:49 +0200

    optimizations

--- bridged-tokens-wrapper/src/lib.rs
+++ bridged-tokens-wrapper/src/lib.rs
@@ -12,12 +12,11 @@ pub trait BridgedTokensWrapper {
         universal_bridged_token_ids: TokenIdentifier,
         #[var_args] chain_specific_tokens: MultiValueEncoded<TokenIdentifier>,
     ) {
-        self.add_wrapped_token(universal_bridged_token_ids.clone());
+        let mut whitelist = self.chain_specific_token_ids(&universal_bridged_token_ids);
+        self.add_wrapped_token(universal_bridged_token_ids);
 
         for token_id in chain_specific_tokens {
-            let _ = self
-                .chain_specific_token_ids(&universal_bridged_token_ids)
-                .insert(token_id);
+            let _ = whitelist.insert(token_id);
         }
     }
 
@@ -36,15 +35,12 @@ pub trait BridgedTokensWrapper {
             .universal_bridged_token_ids()
             .swap_remove(&universal_bridged_token_ids);
 
-        let chain_specific_tokens = &self.chain_specific_token_ids(&universal_bridged_token_ids);
-
+        let mut chain_specific_tokens = self.chain_specific_token_ids(&universal_bridged_token_ids);
         for token in chain_specific_tokens.iter() {
             self.chain_specific_to_universal_mapping(&token).clear();
         }
 
-        let _ = self
-            .chain_specific_token_ids(&universal_bridged_token_ids)
-            .clear();
+        chain_specific_tokens.clear();
     }
 
     #[only_owner]
@@ -54,28 +50,33 @@ pub trait BridgedTokensWrapper {
         chain_specific_token_id: TokenIdentifier,
         universal_bridged_token_ids: TokenIdentifier,
     ) {
-        let _ = self
-            .chain_specific_token_ids(&universal_bridged_token_ids)
-            .insert(chain_specific_token_id.clone());
+        let chain_to_universal_mapper =
+            self.chain_specific_to_universal_mapping(&chain_specific_token_id);
+        require!(
+            chain_to_universal_mapper.is_empty(),
+            "Chain-specific token is already mapped to another universal token"
+        );
+
+        chain_to_universal_mapper.set(&universal_bridged_token_ids);
 
         let _ = self
-            .chain_specific_to_universal_mapping(&chain_specific_token_id)
-            .set(universal_bridged_token_ids);
+            .chain_specific_token_ids(&universal_bridged_token_ids)
+            .insert(chain_specific_token_id);
     }
 
     #[only_owner]
     #[endpoint(blacklistToken)]
     fn blacklist_token(&self, chain_specific_token_id: TokenIdentifier) {
-        let universal_bridged_token_ids = self
-            .chain_specific_to_universal_mapping(&chain_specific_token_id)
-            .get();
+        let chain_to_universal_mapper =
+            self.chain_specific_to_universal_mapping(&chain_specific_token_id);
+
+        let universal_bridged_token_ids = chain_to_universal_mapper.get();
 
         let _ = self
             .chain_specific_token_ids(&universal_bridged_token_ids)
             .swap_remove(&chain_specific_token_id);
 
-        self.chain_specific_to_universal_mapping(&chain_specific_token_id)
-            .clear();
+        chain_to_universal_mapper.clear();
     }
 
     /// Will wrap what it can, and send back the rest unchanged
@@ -90,11 +91,12 @@ pub trait BridgedTokensWrapper {
         let mut new_payments = ManagedVec::new();
 
         for payment in &original_payments {
-            let universal_token_id = self
-                .chain_specific_to_universal_mapping(&payment.token_identifier)
-                .get();
-            let token_whitelist = self.chain_specific_token_ids(&universal_token_id);
-            let new_payment = if token_whitelist.contains(&payment.token_identifier) {
+            let universal_token_id_mapper =
+                self.chain_specific_to_universal_mapping(&payment.token_identifier);
+
+            // if there is chain specific -> universal mapping, then the token is whitelisted
+            let new_payment = if !universal_token_id_mapper.is_empty() {
+                let universal_token_id = universal_token_id_mapper.get();
                 self.send()
                     .esdt_local_mint(&universal_token_id, 0, &payment.amount);
 
@@ -105,7 +107,7 @@ pub trait BridgedTokensWrapper {
                     token_type: EsdtTokenType::Fungible,
                     token_identifier: universal_token_id.clone(),
                     token_nonce: 0,
-                    amount: payment.amount.clone(),
+                    amount: payment.amount,
                 }
             } else {
                 payment
@@ -115,7 +117,6 @@ pub trait BridgedTokensWrapper {
         }
 
         let caller = self.blockchain().get_caller();
-
         self.send().direct_multi(&caller, &new_payments, &[]);
 
         new_payments
@@ -129,6 +130,8 @@ pub trait BridgedTokensWrapper {
         #[payment_amount] payment_amount: BigUint,
         requested_token: TokenIdentifier,
     ) {
+        require!(payment_amount > 0u32, "Must pay more than 0 tokens!");
+
         let universal_bridged_token_ids = self
             .chain_specific_to_universal_mapping(&requested_token)
             .get();
@@ -138,21 +141,19 @@ pub trait BridgedTokensWrapper {
         );
 
         let chain_specific_token_id = &requested_token;
-
-        require!(payment_amount > 0u32, "Must pay more than 0 tokens!");
+        let token_liquidity_mapper = self.token_liquidity(chain_specific_token_id);
+        let liquidity_amount = token_liquidity_mapper.get();
         require!(
-            payment_amount <= self.token_liquidity(chain_specific_token_id).get(),
+            payment_amount <= liquidity_amount,
             "Contract does not have enough funds"
         );
 
-        self.token_liquidity(chain_specific_token_id)
-            .update(|value| *value -= &payment_amount);
+        token_liquidity_mapper.set(&liquidity_amount + &payment_amount);
 
         self.send()
             .esdt_local_burn(&universal_bridged_token_ids, 0, &payment_amount);
 
         let caller = self.blockchain().get_caller();
-
         self.send()
             .direct(&caller, chain_specific_token_id, 0, &payment_amount, &[]);
     }
@@ -161,7 +162,7 @@ pub trait BridgedTokensWrapper {
         let roles = self.blockchain().get_esdt_local_roles(token_id);
 
         require!(
-            roles.has_role(&EsdtLocalRole::Mint) & roles.has_role(&EsdtLocalRole::Burn),
+            roles.has_role(&EsdtLocalRole::Mint) && roles.has_role(&EsdtLocalRole::Burn),
             "Must set local role first"
         );
     }
