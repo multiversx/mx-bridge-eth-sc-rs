542f6205ed9078214ee889c1399ca87a2e149b89
Rebegea Dragos-Alexandru <dragos.rebegea@elrond.com>
2023-03-10 11:45:05 +0200

    update snippets

--- multisig/interaction/config/issue-tokens-snippets.sh
+++ multisig/interaction/config/issue-tokens-snippets.sh
@@ -2,25 +2,25 @@ ESDT_ISSUE_COST=50000000000000000
 
 issueUniversalToken() {
     CHECK_VARIABLES ESDT_SYSTEM_SC_ADDRESS ESDT_ISSUE_COST UNIVERSAL_TOKEN_DISPLAY_NAME \
-    UNIVERSAL_TOKEN_TICKER NR_DECIMALS
+    UNIVERSAL_TOKEN_TICKER NR_DECIMALS_UNIVERSAL
 
-    erdpy --verbose contract call ${ESDT_SYSTEM_SC_ADDRESS} --recall-nonce --pem=${ALICE} \
+    mxpy --verbose contract call ${ESDT_SYSTEM_SC_ADDRESS} --recall-nonce --pem=${ALICE} \
     --gas-limit=60000000 --value=${ESDT_ISSUE_COST} --function="issue" \
     --arguments str:${UNIVERSAL_TOKEN_DISPLAY_NAME} str:${UNIVERSAL_TOKEN_TICKER} \
-    0 ${NR_DECIMALS} str:canAddSpecialRoles str:true \
+    0 ${NR_DECIMALS_UNIVERSAL} str:canAddSpecialRoles str:true \
     --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
 issueChainSpecificToken() {
     CHECK_VARIABLES ESDT_SYSTEM_SC_ADDRESS ESDT_ISSUE_COST CHAIN_SPECIFIC_TOKEN_DISPLAY_NAME \
-    CHAIN_SPECIFIC_TOKEN_TICKER NR_DECIMALS UNIVERSAL_TOKENS_ALREADY_MINTED
+    CHAIN_SPECIFIC_TOKEN_TICKER NR_DECIMALS_CHAIN_SPECIFIC UNIVERSAL_TOKENS_ALREADY_MINTED
     
-    VALUE_TO_MINT=$(echo "$UNIVERSAL_TOKENS_ALREADY_MINTED*10^$NR_DECIMALS" | bc)
+    VALUE_TO_MINT=$(echo "$UNIVERSAL_TOKENS_ALREADY_MINTED*10^$NR_DECIMALS_CHAIN_SPECIFIC" | bc)
 
-    erdpy --verbose contract call ${ESDT_SYSTEM_SC_ADDRESS} --recall-nonce --pem=${ALICE} \
+    mxpy --verbose contract call ${ESDT_SYSTEM_SC_ADDRESS} --recall-nonce --pem=${ALICE} \
     --gas-limit=60000000 --value=${ESDT_ISSUE_COST} --function="issue" \
     --arguments str:${CHAIN_SPECIFIC_TOKEN_DISPLAY_NAME} str:${CHAIN_SPECIFIC_TOKEN_TICKER} \
-    ${VALUE_TO_MINT} ${NR_DECIMALS} str:canAddSpecialRoles str:true \
+    ${VALUE_TO_MINT} ${NR_DECIMALS_CHAIN_SPECIFIC} str:canAddSpecialRoles str:true \
     --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
@@ -29,8 +29,32 @@ transferToSC() {
 
     VALUE_TO_MINT=$(echo "$UNIVERSAL_TOKENS_ALREADY_MINTED*10^$NR_DECIMALS" | bc)
 
-    erdpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
+    mxpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
     --gas-limit=5000000 --function="ESDTTransfer" \
     --arguments str:${CHAIN_SPECIFIC_TOKEN} ${VALUE_TO_MINT} str:depositLiquidity \
+    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+}
+
+setMintRole() {
+    mxpy --verbose contract call ${ESDT_SYSTEM_SC_ADDRESS} --recall-nonce --pem=${ALICE} \
+    --gas-limit=60000000 --function="setSpecialRole" \
+    --arguments str:${CHAIN_SPECIFIC_TOKEN} ${ALICE_ADDRESS} str:ESDTRoleLocalMint \
+    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+}
+
+unSetMintRole() {
+    mxpy --verbose contract call ${ESDT_SYSTEM_SC_ADDRESS} --recall-nonce --pem=${ALICE} \
+    --gas-limit=60000000 --function="unSetSpecialRole" \
+    --arguments str:${CHAIN_SPECIFIC_TOKEN} ${ALICE_ADDRESS} str:ESDTRoleLocalMint \
+    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+}
+
+mint() {
+    CHECK_VARIABLES NR_DECIMALS ALICE_ADDRESS CHAIN_SPECIFIC_TOKEN
+    read -p "Amount to mint(without decimals): " AMOUNT_TO_MINT
+    VALUE_TO_MINT=$(echo "$AMOUNT_TO_MINT*10^$NR_DECIMALS" | bc)
+    mxpy --verbose contract call ${ALICE_ADDRESS} --recall-nonce --pem=${ALICE} \
+    --gas-limit=300000 --function="ESDTLocalMint" \
+    --arguments str:${CHAIN_SPECIFIC_TOKEN} ${VALUE_TO_MINT} \
     --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
--- multisig/interaction/config/wrapped-snippets.sh
+++ multisig/interaction/config/wrapped-snippets.sh
@@ -8,15 +8,15 @@
 deployBridgedTokensWrapper() {
     CHECK_VARIABLES BRIDGED_TOKENS_WRAPPER_WASM
     
-    erdpy --verbose contract deploy --bytecode=${BRIDGED_TOKENS_WRAPPER_WASM} --recall-nonce --pem=${ALICE} \
+    mxpy --verbose contract deploy --bytecode=${BRIDGED_TOKENS_WRAPPER_WASM} --recall-nonce --pem=${ALICE} \
     --gas-limit=30000000 \
     --send --outfile="deploy-bridged-tokens-wrapper-testnet.interaction.json" --proxy=${PROXY} --chain=${CHAIN_ID} || return
 
-    TRANSACTION=$(erdpy data parse --file="./deploy-bridged-tokens-wrapper-testnet.interaction.json" --expression="data['emittedTransactionHash']")
-    ADDRESS=$(erdpy data parse --file="./deploy-bridged-tokens-wrapper-testnet.interaction.json" --expression="data['contractAddress']")
+    TRANSACTION=$(mxpy data parse --file="./deploy-bridged-tokens-wrapper-testnet.interaction.json" --expression="data['emittedTransactionHash']")
+    ADDRESS=$(mxpy data parse --file="./deploy-bridged-tokens-wrapper-testnet.interaction.json" --expression="data['contractAddress']")
 
-    erdpy data store --key=address-testnet-bridged-tokens-wrapper --value=${ADDRESS}
-    erdpy data store --key=deployTransaction-testnet --value=${TRANSACTION}
+    mxpy data store --key=address-testnet-bridged-tokens-wrapper --value=${ADDRESS}
+    mxpy data store --key=deployTransaction-testnet --value=${TRANSACTION}
 
     echo ""
     echo "Bridged tokens wrapper SC: ${ADDRESS}"
@@ -25,34 +25,96 @@ deployBridgedTokensWrapper() {
 setLocalRolesBridgedTokensWrapper() {
     CHECK_VARIABLES ESDT_SYSTEM_SC_ADDRESS UNIVERSAL_TOKEN BRIDGED_TOKENS_WRAPPER
     
-    erdpy --verbose contract call ${ESDT_SYSTEM_SC_ADDRESS} --recall-nonce --pem=${ALICE} \
+    mxpy --verbose contract call ${ESDT_SYSTEM_SC_ADDRESS} --recall-nonce --pem=${ALICE} \
     --gas-limit=60000000 --function="setSpecialRole" \
     --arguments str:${UNIVERSAL_TOKEN} ${BRIDGED_TOKENS_WRAPPER} str:ESDTRoleLocalMint str:ESDTRoleLocalBurn\
     --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
+unsetLocalRolesBridgedTokensWrapper() {
+    CHECK_VARIABLES ESDT_SYSTEM_SC_ADDRESS UNIVERSAL_TOKEN BRIDGED_TOKENS_WRAPPER
+    
+    mxpy --verbose contract call ${ESDT_SYSTEM_SC_ADDRESS} --recall-nonce --pem=${ALICE} \
+    --gas-limit=60000000 --function="unSetSpecialRole" \
+    --arguments str:${UNIVERSAL_TOKEN} ${BRIDGED_TOKENS_WRAPPER} str:ESDTRoleLocalMint str:ESDTRoleLocalBurn\
+    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+}
+
 addWrappedToken() {
+    CHECK_VARIABLES BRIDGED_TOKENS_WRAPPER UNIVERSAL_TOKEN NR_DECIMALS_UNIVERSAL
+
+    mxpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
+    --gas-limit=6000000 --function="addWrappedToken" \
+    --arguments str:${UNIVERSAL_TOKEN} ${NR_DECIMALS_UNIVERSAL} \
+    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+}
+
+removeWrappedToken() {
     CHECK_VARIABLES BRIDGED_TOKENS_WRAPPER UNIVERSAL_TOKEN
 
-    erdpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
-    --gas-limit=6000000 --function="addWrappedToken" \
+    mxpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
+    --gas-limit=6000000 --function="removeWrappedToken" \
     --arguments str:${UNIVERSAL_TOKEN} \
     --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
 wrapper-whitelistToken() {
-    CHECK_VARIABLES BRIDGED_TOKENS_WRAPPER CHAIN_SPECIFIC_TOKEN UNIVERSAL_TOKEN
+    CHECK_VARIABLES BRIDGED_TOKENS_WRAPPER CHAIN_SPECIFIC_TOKEN NR_DECIMALS_CHAIN_SPECIFIC UNIVERSAL_TOKEN
 
-    erdpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
+    mxpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
     --gas-limit=6000000 --function="whitelistToken" \
-    --arguments str:${CHAIN_SPECIFIC_TOKEN} str:${UNIVERSAL_TOKEN} \
+    --arguments str:${CHAIN_SPECIFIC_TOKEN} ${NR_DECIMALS_CHAIN_SPECIFIC} str:${UNIVERSAL_TOKEN} \
     --send --proxy=${PROXY} --chain=${CHAIN_ID}
 }
 
-upgradeBridgedTokensWrapper() {
+wrapper-blacklistToken() {
+    CHECK_VARIABLES BRIDGED_TOKENS_WRAPPER CHAIN_SPECIFIC_TOKEN UNIVERSAL_TOKEN
+
+    mxpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
+    --gas-limit=6000000 --function="blacklistToken" \
+    --arguments str:${CHAIN_SPECIFIC_TOKEN} \
+    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+}
+
+wrapper-updateWrappedToken() {
+    CHECK_VARIABLES BRIDGED_TOKENS_WRAPPER UNIVERSAL_TOKEN NR_DECIMALS_UNIVERSAL
+
+    mxpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
+    --gas-limit=6000000 --function="updateWrappedToken" \
+    --arguments str:${UNIVERSAL_TOKEN} ${NR_DECIMALS_UNIVERSAL} \
+    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+}
+
+wrapper-updateWhitelistedToken() {
+    CHECK_VARIABLES BRIDGED_TOKENS_WRAPPER CHAIN_SPECIFIC_TOKEN NR_DECIMALS_CHAIN_SPECIFIC
+
+    mxpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
+    --gas-limit=6000000 --function="blacklistToken" \
+    --arguments str:${CHAIN_SPECIFIC_TOKEN} ${NR_DECIMALS_CHAIN_SPECIFIC} \
+    --send --proxy=${PROXY} --chain=${CHAIN_ID}
+}
+
+
+wrapper-unpause() {
+    CHECK_VARIABLES BRIDGED_TOKENS_WRAPPER
+
+    mxpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
+    --gas-limit=5000000 --function="unpause" \
+    --send --proxy=${PROXY} --chain=${CHAIN_ID} || return
+}
+
+wrapper-pause() {
+    CHECK_VARIABLES BRIDGED_TOKENS_WRAPPER
+
+    mxpy --verbose contract call ${BRIDGED_TOKENS_WRAPPER} --recall-nonce --pem=${ALICE} \
+    --gas-limit=5000000 --function="pause" \
+    --send --proxy=${PROXY} --chain=${CHAIN_ID} || return
+}
+
+wrapper-upgrade() {
     CHECK_VARIABLES BRIDGED_TOKENS_WRAPPER BRIDGED_TOKENS_WRAPPER_WASM
 
-    erdpy --verbose contract upgrade ${BRIDGED_TOKENS_WRAPPER} --bytecode=${BRIDGED_TOKENS_WRAPPER_WASM} --recall-nonce --pem=${ALICE} \
+    mxpy --verbose contract upgrade ${BRIDGED_TOKENS_WRAPPER} --bytecode=${BRIDGED_TOKENS_WRAPPER_WASM} --recall-nonce --pem=${ALICE} \
     --gas-limit=50000000 --send \
     --outfile="upgrade-bridged-tokens-wrapper.json" --proxy=${PROXY} --chain=${CHAIN_ID} || return 
 }
