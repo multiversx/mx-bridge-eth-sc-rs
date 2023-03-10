fa56edee9766a6b6b095f18a77dd66e1511d98df
Alin Cruceat <alyn_509@yahoo.com>
2023-03-10 13:09:14 +0200

    clippy 2

--- bridged-tokens-wrapper/tests/dfp_big_uint_test.rs
+++ bridged-tokens-wrapper/tests/dfp_big_uint_test.rs
@@ -1,25 +1,24 @@
 #![feature(associated_type_bounds)]
 use bridged_tokens_wrapper::DFPBigUint;
-use elrond_wasm::types::BigUint;
 use elrond_wasm_debug::DebugApi;
 
 #[test]
 fn test_biguint() {
     let _ = DebugApi::dummy();
-    let raw = BigUint::<DebugApi>::from(123456u64);
-    let dfp = DFPBigUint::from_raw(raw.clone(), 6);
+    let raw = 123456u64;
+    let dfp = DFPBigUint::<DebugApi>::from_raw(raw.into(), 6);
     let converted = dfp.convert(9);
     assert!(dfp.trunc() == converted.trunc());
-    assert!(converted.convert(9).to_raw() == BigUint::<DebugApi>::from(123456000u64));
-    assert!(converted.convert(1).to_raw() == BigUint::<DebugApi>::from(1u64));
-    assert!(converted.convert(3).to_raw() == BigUint::<DebugApi>::from(123u64));
-    assert!(converted.convert(5).to_raw() == BigUint::<DebugApi>::from(12345u64));
+    assert!(converted.convert(9).to_raw() == 123456000u64);
+    assert!(converted.convert(1).to_raw() == 1u64);
+    assert!(converted.convert(3).to_raw() == 123u64);
+    assert!(converted.convert(5).to_raw() == 12345u64);
 }
 
 #[test]
 fn test_mandos_scenario_values() {
     let _ = DebugApi::dummy();
-    let raw = BigUint::<DebugApi>::from(300000000000000u64);
-    let dfp = DFPBigUint::from_raw(raw.clone(), 18);
-    assert!(dfp.convert(6).to_raw() == BigUint::<DebugApi>::from(300u64));
+    let raw = 300000000000000u64;
+    let dfp = DFPBigUint::<DebugApi>::from_raw(raw.into(), 18);
+    assert!(dfp.convert(6).to_raw() == 300u64);
 }
