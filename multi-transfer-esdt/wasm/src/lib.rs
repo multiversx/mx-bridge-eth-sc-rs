dbf666851018fbe8a0856b48958b92244e054fe5
Costin Carabaș <costin.carabas@gmail.com>
2023-12-27 13:14:18 +0200

    Framework upgrade bridge-proxy to 0.45.2

--- multi-transfer-esdt/wasm/src/lib.rs
+++ multi-transfer-esdt/wasm/src/lib.rs
@@ -5,9 +5,9 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                           17
+// Endpoints:                           18
 // Async Callback (empty):               1
-// Total number of exported functions:  19
+// Total number of exported functions:  20
 
 #![no_std]
 
