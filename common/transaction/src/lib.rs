80fe43f01d54c95371112db3df72d7186f3f798c
Costin Carabaș <costin.carabas@gmail.com>
2024-08-01 14:21:45 +0300

    Framework upgrade 0.52.1

--- common/transaction/src/lib.rs
+++ common/transaction/src/lib.rs
@@ -1,6 +1,5 @@
 #![no_std]
 
-use codec::NestedDecodeInput;
 use multiversx_sc::derive_imports::*;
 use multiversx_sc::imports::*;
 
@@ -27,98 +26,98 @@ pub type PaymentsVec<M> = ManagedVec<M, EsdtTokenPayment<M>>;
 pub type TxBatchSplitInFields<M> = MultiValue2<u64, MultiValueEncoded<M, TxAsMultiValue<M>>>;
 
 #[type_abi]
-#[derive(NestedEncode, TopDecode, TopEncode, Clone, ManagedVecItem)]
+#[derive(NestedEncode, NestedDecode, TopDecode, TopEncode, Clone, ManagedVecItem)]
 pub struct CallData<M: ManagedTypeApi> {
     pub endpoint: ManagedBuffer<M>,
     pub gas_limit: u64,
     pub args: ManagedOption<M, ManagedVec<M, ManagedBuffer<M>>>,
 }
 
-impl<M: ManagedTypeApi> Default for CallData<M> {
-    fn default() -> Self {
-        CallData {
-            endpoint: ManagedBuffer::new(),
-            gas_limit: 0u64,
-            args: ManagedOption::none(),
-        }
-    }
-}
+// impl<M: ManagedTypeApi> Default for CallData<M> {
+//     fn default() -> Self {
+//         CallData {
+//             endpoint: ManagedBuffer::new(),
+//             gas_limit: 0u64,
+//             args: ManagedOption::none(),
+//         }
+//     }
+// }
 
-impl<M: ManagedTypeApi> NestedDecode for CallData<M> {
-    fn dep_decode_or_handle_err<I, H>(nested_buffer: &mut I, h: H) -> Result<Self, H::HandledErr>
-    where
-        I: codec::NestedDecodeInput,
-        H: codec::DecodeErrorHandler,
-    {
-        let mut endpoint = ManagedBuffer::new();
-        if !nested_buffer.is_depleted() {
-            endpoint = ManagedBuffer::dep_decode_or_handle_err(nested_buffer, h)?;
-        }
+// impl<M: ManagedTypeApi> NestedDecode for CallData<M> {
+//     fn dep_decode_or_handle_err<I, H>(nested_buffer: &mut I, h: H) -> Result<Self, H::HandledErr>
+//     where
+//         I: codec::NestedDecodeInput,
+//         H: codec::DecodeErrorHandler,
+//     {
+//         let mut endpoint = ManagedBuffer::new();
+//         if !nested_buffer.is_depleted() {
+//             endpoint = ManagedBuffer::dep_decode_or_handle_err(nested_buffer, h)?;
+//         }
 
-        let mut gas_limit = 0u64;
-        if !nested_buffer.is_depleted() {
-            gas_limit = u64::dep_decode_or_handle_err(nested_buffer, h)?;
-        }
+//         let mut gas_limit = 0u64;
+//         if !nested_buffer.is_depleted() {
+//             gas_limit = u64::dep_decode_or_handle_err(nested_buffer, h)?;
+//         }
 
-        let mut args: ManagedVec<M, ManagedBuffer<M>> = ManagedVec::new();
-        while !nested_buffer.is_depleted() {
-            args.push(ManagedBuffer::dep_decode_or_handle_err(nested_buffer, h)?);
-        }
+//         let mut args: ManagedVec<M, ManagedBuffer<M>> = ManagedVec::new();
+//         while !nested_buffer.is_depleted() {
+//             args.push(ManagedBuffer::dep_decode_or_handle_err(nested_buffer, h)?);
+//         }
 
-        let args: ManagedOption<M, ManagedVec<M, ManagedBuffer<M>>> = if args.is_empty() {
-            ManagedOption::none()
-        } else {
-            ManagedOption::some(args)
-        };
+//         let args: ManagedOption<M, ManagedVec<M, ManagedBuffer<M>>> = if args.is_empty() {
+//             ManagedOption::none()
+//         } else {
+//             ManagedOption::some(args)
+//         };
 
-        Result::Ok(CallData {
-            endpoint,
-            gas_limit,
-            args,
-        })
-    }
-}
+//         Result::Ok(CallData {
+//             endpoint,
+//             gas_limit,
+//             args,
+//         })
+//     }
+// }
 
 #[type_abi]
-#[derive(NestedEncode, NestedDecode, TopEncode, Clone, ManagedVecItem)]
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, Clone)]
 pub struct EthTransaction<M: ManagedTypeApi> {
     pub from: EthAddress<M>,
     pub to: ManagedAddress<M>,
     pub token_id: TokenIdentifier<M>,
     pub amount: BigUint<M>,
     pub tx_nonce: TxNonce,
-    pub call_data: CallData<M>,
+    pub call_data: ManagedBufferReadToEnd<M>,
 }
 
-impl<M: ManagedTypeApi> TopDecode for EthTransaction<M> {
-    fn top_decode_or_handle_err<I, H>(input: I, h: H) -> Result<Self, H::HandledErr>
-    where
-        I: codec::TopDecodeInput,
-        H: codec::DecodeErrorHandler,
-    {
-        let mut nested_buffer = input.into_nested_buffer();
-        let from = EthAddress::dep_decode_or_handle_err(&mut nested_buffer, h)?;
-        let to = ManagedAddress::dep_decode_or_handle_err(&mut nested_buffer, h)?;
-        let token_id = TokenIdentifier::dep_decode_or_handle_err(&mut nested_buffer, h)?;
-        let amount = BigUint::dep_decode_or_handle_err(&mut nested_buffer, h)?;
-        let tx_nonce = TxNonce::dep_decode_or_handle_err(&mut nested_buffer, h)?;
+// impl<M: ManagedTypeApi> TopDecode for EthTransaction<M> {
+//     fn top_decode_or_handle_err<I, H>(input: I, h: H) -> Result<Self, H::HandledErr>
+//     where
+//         I: codec::TopDecodeInput,
+//         H: codec::DecodeErrorHandler,
+//     {
+//         let mut nested_buffer = input.into_nested_buffer();
+//         let from = EthAddress::dep_decode_or_handle_err(&mut nested_buffer, h)?;
+//         let to = ManagedAddress::dep_decode_or_handle_err(&mut nested_buffer, h)?;
+//         let token_id = TokenIdentifier::dep_decode_or_handle_err(&mut nested_buffer, h)?;
+//         let amount = BigUint::dep_decode_or_handle_err(&mut nested_buffer, h)?;
+//         let tx_nonce = TxNonce::dep_decode_or_handle_err(&mut nested_buffer, h)?;
 
-        let mut call_data = CallData::default();
+//         let mut call_data = CallData::default();
 
-        if !nested_buffer.is_depleted() {
-            call_data = CallData::dep_decode_or_handle_err(&mut nested_buffer, h)?;
-        }
+//         if !nested_buffer.is_depleted() {
+//             call_data = CallData::dep_decode_or_handle_err(&mut nested_buffer, h)?;
+//         }
 
-        Result::Ok(EthTransaction {
-            from,
-            to,
-            token_id,
-            amount,
-            tx_nonce,
-            call_data,
-        })
-    }
-}
+//         Result::Ok(EthTransaction {
+//             from,
+//             to,
+//             token_id,
+//             amount,
+//             tx_nonce,
+//             call_data,
+//         })
+//     }
+// }
 
 #[type_abi]
 #[derive(TopEncode, TopDecode, NestedEncode, NestedDecode, ManagedVecItem, Clone)]
