c2f26562667f6e99f2ae463cf2eede4caf31c64d
Iulian Pascalau <iulian.pascalau@gmail.com>
2024-08-12 14:54:24 +0300

    - added more endpoints on test caller contract

--- test-caller/src/test-caller.rs
+++ test-caller/src/test-caller.rs
@@ -1,5 +1,17 @@
 #![no_std]
 
+use multiversx_sc::api::ManagedTypeApi;
+use multiversx_sc::derive_imports::*;
+use multiversx_sc::imports::*;
+
+#[type_abi]
+#[derive(NestedEncode, NestedDecode, TopEncode, TopDecode, Clone, ManagedVecItem)]
+pub struct CalledData<M: ManagedTypeApi> {
+    pub size:             u64,
+    pub address:          ManagedAddress<M>,
+    pub token_identifier: TokenIdentifier<M>,
+}
+
 #[multiversx_sc::contract]
 pub trait TestCallerContract {
     #[init]
@@ -14,4 +26,26 @@ pub trait TestCallerContract {
 
     #[endpoint(callNonPayable)]
     fn call_non_payable(&self) {}
+
+    #[payable("*")]
+    #[view(callPayableWithParams)]
+    fn call_payable_with_params(
+        &self,
+        size: u64,
+        address: ManagedAddress,
+    ) {
+        let payment = self.call_value().single_esdt();
+        let token_identifier = payment.token_identifier;
+
+        let data = CalledData{
+            size,
+            address,
+            token_identifier,
+        };
+
+        _ = self.called_data_params().push(&data);
+    }
+
+    #[storage_mapper("calledDataParams")]
+    fn called_data_params(&self) -> VecMapper<CalledData<Self::Api>>;
 }
