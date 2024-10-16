5c1e6ae5cfbf508f29d39d818f556b14321be9bb
Iulian Pascalau <iulian.pascalau@gmail.com>
2024-10-16 15:44:55 +0300

    - added the proxy contract on the upgrade function

--- multisig/src/esdt_safe_proxy.rs
+++ multisig/src/esdt_safe_proxy.rs
@@ -80,18 +80,21 @@ where
     pub fn upgrade<
         Arg0: ProxyArg<ManagedAddress<Env::Api>>,
         Arg1: ProxyArg<ManagedAddress<Env::Api>>,
-        Arg2: ProxyArg<BigUint<Env::Api>>,
+        Arg2: ProxyArg<ManagedAddress<Env::Api>>,
+        Arg3: ProxyArg<BigUint<Env::Api>>,
     >(
         self,
         fee_estimator_contract_address: Arg0,
         multi_transfer_contract_address: Arg1,
-        eth_tx_gas_limit: Arg2,
+        bridge_proxy_contract_address: Arg2,
+        eth_tx_gas_limit: Arg3,
     ) -> TxTypedUpgrade<Env, From, To, NotPayable, Gas, ()> {
         self.wrapped_tx
             .payment(NotPayable)
             .raw_upgrade()
             .argument(&fee_estimator_contract_address)
             .argument(&multi_transfer_contract_address)
+            .argument(&bridge_proxy_contract_address)
             .argument(&eth_tx_gas_limit)
             .original_result()
     }
