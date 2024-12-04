8c650601e1d2599d7dc82086109ba82ed795f27d
Iulian Pascalau <36724586+iulianpascalau@users.noreply.github.com>
2024-12-04 17:04:17 +0200

    Added sync value for safe contract script (#262)
    * - added sync value for safe contract
    
    * - added empty line
    
    * - added empty line

--- multisig/interaction/script.sh
+++ multisig/interaction/script.sh
@@ -127,6 +127,10 @@ case "$1" in
   confirmation pause-v2-contracts
   ;;
 
+'sync-value-with-eth-denom')
+  confirmation sync-value-with-eth-denom
+  ;;
+
 *)
   echo "Usage: Invalid choice: '"$1"'" 
   echo -e 
@@ -136,7 +140,7 @@ case "$1" in
   echo "    \"pause-contracts\", \"unpause-contracts\", \"add-relayer\", \"remove-relayer\", \"pause-v2-contracts\", "
   echo "    \"set-safe-max-tx\", \"set-safe-batch-block-duration\", \"change-quorum\", \"set-swap-fee\", "
   echo "    \"whitelist-token\", \"whitelist-native-token\", \"remove-whitelist-token\", \"upgrade-wrapper-universal-token\", \"upgrade-wrapper-chain-specific-token\", "
-  echo "    \"mint-chain-specific\", \"init-supply-mint-burn\", "
+  echo "    \"mint-chain-specific\", \"init-supply-mint-burn\", \"sync-value-with-eth-denom\", "
   echo "    \"faucet-deposit\", \"deploy-test-caller\""
   echo "  } "
   ;;
