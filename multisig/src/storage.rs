bb17ec2f727fc70e4e037f066e4d1e20add04327
Costin Carabaș <costin.carabas@gmail.com>
2025-03-03 13:30:53 +0200

    multisig: Revert storage mappers from camelCase to snake_case

--- multisig/src/storage.rs
+++ multisig/src/storage.rs
@@ -24,13 +24,13 @@ pub trait StorageModule {
     /// Denormalized board member count.
     /// It is kept in sync with the user list by the contract.
     #[view(getNumBoardMembers)]
-    #[storage_mapper("numBoardMembers")]
+    #[storage_mapper("num_board_members")]
     fn num_board_members(&self) -> SingleValueMapper<usize>;
 
-    #[storage_mapper("actionData")]
+    #[storage_mapper("action_data")]
     fn action_mapper(&self) -> VecMapper<Action<Self::Api>>;
 
-    #[storage_mapper("actionSignerIds")]
+    #[storage_mapper("action_signer_ids")]
     fn action_signer_ids(&self, action_id: usize) -> UnorderedSetMapper<usize>;
 
     /// The required amount to stake for accepting relayer position
